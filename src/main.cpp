e7b642f526ca63265b299e4b92a387dd5f22ec04
Giray <giray@gokirmak.gen.tr>
2025-02-11 14:40:08 +0300

    choke removed

--- src/main.cpp
+++ src/main.cpp
@@ -2,19 +2,16 @@
 
 // Define the pin numbers for the onOffSwitch, choke, ignition, and contactor
 const int onOffSwitchPin = 16; 
-const int chokePin = 14; 
 const int ignitionPin = 12; 
 const int contactorPin = 13;  
 const int buttonPin = 15; 
 
 // Define the relay states and timings
 bool onOffSwitchState = LOW;
-bool chokeState = LOW;     
 bool ignitionState = LOW;  
 bool contactorState = LOW;  
 
 unsigned long startTime;
-unsigned long chokeOnTime = 6000;    // Stay on for 6s total
 unsigned long ignitionOnTime = 3000;  // Ignition stays on for 3s
 unsigned long ignitionTriggerDelay = 1000; // Ignition triggered 1s after boot
 unsigned long contactorOnTime = 60000; // 60s after boot
@@ -22,7 +19,6 @@ unsigned long engineOffOnTime = 50000; // 50s after button is pressed
 unsigned long contactorOffOnTime = 3000; // 3s after button is pressed
 
 bool buttonPressed = false;
-bool chokeTriggered = false;
 bool ignitionTriggered = false;
 unsigned long buttonPressTime = 0;
 int buttonState = HIGH;
@@ -30,7 +26,6 @@ int buttonState = HIGH;
 void setup() {
   // Initialize the relay pins and the button pin as inputs and outputs
   pinMode(onOffSwitchPin, OUTPUT);
-  pinMode(chokePin, OUTPUT);
   pinMode(ignitionPin, OUTPUT);
   pinMode(contactorPin, OUTPUT);
   pinMode(buttonPin, INPUT); // Use internal pull-up resistor
@@ -47,12 +42,6 @@ void setup() {
     digitalWrite(onOffSwitchPin, HIGH); // Keep the onOffSwitch on
     onOffSwitchState = HIGH;
     Serial.println("onOffSwitch turned on.");  
-
-    Serial.println("Turning on choke.");
-    digitalWrite(chokePin, HIGH); // Turn on the choke
-    chokeState = HIGH;
-    chokeTriggered = true;
-    Serial.println("Choke turned on.");
   }
 
   // Serial output to indicate setup complete
@@ -80,14 +69,6 @@ void loop() {
       Serial.println("onOffSwitch turned off.");
     }
   } 
-
-  // Check if it's time to turn off the choke
-  if (chokeTriggered && chokeState == HIGH && currentTime - startTime >= chokeOnTime) {
-    Serial.println("Turning off choke.");
-    digitalWrite(chokePin, LOW); // Turn off the choke
-    chokeState = LOW;
-    Serial.println("Choke turned off.");
-  }
 
   // Turn on the ignition 1500ms after boot if not already triggered
   if (!ignitionTriggered && currentTime - startTime >= ignitionTriggerDelay && buttonState == HIGH) {
