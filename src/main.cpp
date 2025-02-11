095a48259541455f994e8ea0e6a92b29b017ce2e
Giray <giray@gokirmak.gen.tr>
2025-02-11 15:48:19 +0300

    button --> mains

--- src/main.cpp
+++ src/main.cpp
@@ -4,7 +4,7 @@
 const int onOffSwitchPin = 16; 
 const int ignitionPin = 12; 
 const int contactorPin = 13;  
-const int buttonPin = 15; 
+const int mainsPin = 15; 
 
 // Define the relay states and timings
 bool onOffSwitchState = LOW;
@@ -15,29 +15,29 @@ unsigned long startTime;
 unsigned long ignitionOnTime = 3000;  // Ignition stays on for 3s
 unsigned long ignitionTriggerDelay = 1000; // Ignition triggered 1s after boot
 unsigned long contactorOnTime = 60000; // 60s after boot
-unsigned long engineOffOnTime = 45000; // 45s after button is pressed
-unsigned long buttonPressedOnTime = 5000; // 10s after button is pressed
+unsigned long engineOffOnTime = 45000; // 45s after mains is alive
+unsigned long mainsOnTime = 5000; // 5s after mains is alive
 
-bool buttonPressed = false;
+bool mains = false;
 bool ignitionTriggered = false;
-unsigned long buttonPressTime = 0;
-int buttonState = HIGH;
+unsigned long mainsTime = 0;
+int mainsState = HIGH;
 
 void setup() {
-  // Initialize the relay pins and the button pin as inputs and outputs
+  // Initialize the relay pins and the mains pin as inputs and outputs
   pinMode(onOffSwitchPin, OUTPUT);
   pinMode(ignitionPin, OUTPUT);
   pinMode(contactorPin, OUTPUT);
-  pinMode(buttonPin, INPUT); // Use internal pull-up resistor
+  pinMode(mainsPin, INPUT); 
 
   // Initialize Serial communication
   Serial.begin(115200);
 
   // Record the start time
   startTime = millis();
-  buttonState = digitalRead(buttonPin);
+  mainsState = digitalRead(mainsPin);
 
-  if (buttonState == HIGH) {
+  if (mainsState == HIGH) {
     Serial.println("Turning on onOffSwitch.");
     digitalWrite(onOffSwitchPin, HIGH); // Keep the onOffSwitch on
     onOffSwitchState = HIGH;
@@ -51,26 +51,26 @@ void setup() {
 
 void loop() {
   unsigned long currentTime = millis();
-  buttonState = digitalRead(buttonPin);
+  mainsState = digitalRead(mainsPin);
 
-  if (buttonState == HIGH) {
-    buttonPressTime=0;
+  if (mainsState == HIGH) {
+    mainsTime=0;
   }
 
-  // Check if the button is pressed to start counting engineOffOnTime
-  if (buttonState == LOW && buttonPressTime==0) {
-    buttonPressTime = currentTime;
-    Serial.println("Button pressed. Starting countdown.");
+  // Check if the mains is alive to start counting engineOffOnTime
+  if (mainsState == LOW && mainsTime==0) {
+    mainsTime = currentTime;
+    Serial.println("Mains alive. Starting countdown.");
   }
 
-  if (!buttonPressed && buttonState == LOW && (currentTime - buttonPressTime >= buttonPressedOnTime)) { 
-    buttonPressed=true;
-    buttonPressTime = currentTime;
-    Serial.println("Button pressed countdown finished. Starting engineOffOnTime countdown.");
+  if (!mains && mainsState == LOW && (currentTime - mainsTime >= mainsOnTime)) { 
+    mains=true;
+    mainsTime = currentTime;
+    Serial.println("Mains alive countdown finished. Starting engineOffOnTime countdown.");
   }
 
-  // Turn off the onOffSwitch 50 seconds after the button is pressed
-  if (buttonPressed && (currentTime - buttonPressTime >= engineOffOnTime)) {
+  // Turn off the onOffSwitch 50 seconds after the mains is alive
+  if (mains && (currentTime - mainsTime >= engineOffOnTime)) {
     if (onOffSwitchState == HIGH) {
       Serial.println("Turning off onOffSwitch.");
       digitalWrite(onOffSwitchPin, LOW); // Turn off the onOffSwitch
@@ -80,7 +80,7 @@ void loop() {
   } 
 
   // Turn on the ignition 1500ms after boot if not already triggered
-  if (!buttonPressed && !ignitionTriggered && currentTime - startTime >= ignitionTriggerDelay && buttonState == HIGH) {
+  if (!mains && !ignitionTriggered && currentTime - startTime >= ignitionTriggerDelay && mainsState == HIGH) {
     Serial.println("Turning on ignition.");
     digitalWrite(ignitionPin, HIGH); // Turn on the ignition
     ignitionState = HIGH;
@@ -97,14 +97,14 @@ void loop() {
   }
 
   // Check if it's time to turn on the contactor
-  if (!buttonPressed && currentTime - startTime >= contactorOnTime && buttonState == HIGH && contactorState == LOW && onOffSwitchState == HIGH) {
+  if (!mains && currentTime - startTime >= contactorOnTime && mainsState == HIGH && contactorState == LOW && onOffSwitchState == HIGH) {
     Serial.println("Turning on contactor.");
     digitalWrite(contactorPin, HIGH); // Turn on the contactor
     contactorState = HIGH;
     Serial.println("Contactor turned on.");
-  } else if (buttonPressed && buttonState == LOW && contactorState == HIGH) {
+  } else if (mains && mainsState == LOW && contactorState == HIGH) {
     Serial.println("Turning off contactor.");
-    digitalWrite(contactorPin, LOW); // Turn off the contactor if the button is pressed
+    digitalWrite(contactorPin, LOW); // Turn off the contactor if the mains is alive
     contactorState = LOW;
     Serial.println("Contactor turned off.");
   }
